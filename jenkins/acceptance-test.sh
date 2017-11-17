5ffad730f6c9ce2b18a80259ee31ade51336b3d3
Minno Dang <minh.ngocdang@digital.cabinet-office.gov.uk>
2017-11-17 15:30:53 +0000

    TT-1526: fixed expected hub url on PaaS for acceptance test
    Mob: @vixus0 @alan-gds @minhngocd

--- jenkins/acceptance-test.sh
+++ jenkins/acceptance-test.sh
@@ -3,5 +3,5 @@
 set -e
 
 export PROXY_NODE_URL="https://verify-eidas-notification.cloudapps.digital"
-export HUB_URL="http://stub-idp"
+export HUB_URL="https://verify-eidas-notification.cloudapps.digital/stub-hub"
 ./gradlew clean acceptanceTest
