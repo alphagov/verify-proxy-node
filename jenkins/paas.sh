f8dcfe8198ca5dd778426d93329917cb6be4cbcd
Minno Dang <minh.ngocdang@digital.cabinet-office.gov.uk>
2017-12-06 19:18:50 +0000

    TT-1638: Configurations and script to deploy Stub IDP to PaaS
    In order to deploy an instance of StubIDP that would work with our Proxy
    Node, we need to configure StubIDP with Proxy-Node specific values and
    deploy to PaaS from the given manifest.yml

--- jenkins/paas.sh
+++ jenkins/paas.sh
@@ -1,32 +1,6 @@
 #!/usr/bin/env bash
 
-set -eu
-
-CF_API="api.cloud.service.gov.uk"
-CF_ORG="govuk-verify"
-CF_SPACE="notification"
-
-if [ -z "${CF_USER:-}" ]; then
-  # For running locally
-  echo "CF_USER env var is unset so assume we're running locally" >&2
-else
-  # CloudFoundry will cache credentials in ~/.cf/config.json by default.
-  # Set CF_HOME in a temp dir so that jobs do not share or overwrite each others' credentials.
-  export CF_HOME="$(mktemp -d)"
-  mkdir -p "$CF_HOME"
-
-  # Make sure credentials have been set
-  CF_PASS="${CF_PASS:?CF_PASS environment variable needs to be set}"
-
-  # Remove working directory and auth token on forced exit
-  trap 'rm -r $CF_HOME' EXIT
-  trap 'cf logout' EXIT
-
-  echo "Authenticating to CloudFoundry at '$CF_API' ($CF_ORG/$CF_SPACE) as '$CF_USER'" >&2
-  cf api "$CF_API"
-  cf auth "$CF_USER" "$CF_PASS"
-  cf target -o "$CF_ORG" -s "$CF_SPACE"
-fi
+./jenkins/login_to_paas.sh
 
 ./gradlew -x test pushToPaas
 ./jenkins/acceptance-test.sh
