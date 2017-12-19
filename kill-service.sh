84bf69b2a1bd63a1fc41e34c862f09a4ef0464c4
Minno Dang <minh.ngocdang@digital.cabinet-office.gov.uk>
2017-12-19 15:13:40 +0000

    TT-1639: Fixed kill script to kill Proxy Node and Stub IDP processes
    The script was caught in a non-ending loop. Need to check for running
    PIDs before we can execute a kill.
    
    Pair: @cobainc0 @minhngocd

--- kill-service.sh
+++ kill-service.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
+proxy_node_pids="jps | awk '/EidasProxyNodeApplication/ { print \$1 }'"
+stub_idp_pids="jps | awk '/StubIdpApplication/ { print \$1 }'"
+
 # Kill locally running proxy node
 
 printf "Killing Proxy Node ... "
-while jps | awk '/EidasProxyNodeApplication/ { print $1 }' | xargs kill 2>/dev/null; do 
+while [ -n "$(eval $proxy_node_pids)" ]; do
+  printf "."
+  eval $proxy_node_pids | xargs kill 2>/dev/null
   sleep 1
 done
 echo "KILLED"
 
 printf "Killing Stub IDP ... "
-while jps | awk '/StubIdpApplication/ { print $1 }' | xargs kill 2>/dev/null; do
+while [ -n "$(eval $stub_idp_pids)" ]; do
+  printf "."
+  eval $stub_idp_pids | xargs kill 2>/dev/null
   sleep 1
 done
 echo "KILLED"
