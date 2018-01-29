1049c268ccd7fc2398ea39eb48e8bd17c039869f
Adrian Wong <awong@thoughtworks.com>
2018-01-29 15:41:44 +0000

    TT-1707: Added KeyInfo to Encrypted data on eIDAS Response
    - ConnectorNodeMetadata now returns X509Credential directly rather than an EncryptionCredential wrapper.
    - The EncryptionCredential wrapper has been removed
    - CEF ref requires X509 certificate to be included in the KeyInfo of EncryptionKeys
    
    Authors: @adrianw1832 @ejrowley @vixus0 @minhngocd

--- src/main/java/uk/gov/ida/notification/pki/EncryptionCredential.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.ida.notification.pki;
-
-import org.opensaml.security.credential.BasicCredential;
-
-import java.security.PublicKey;
-
-public class EncryptionCredential extends BasicCredential {
-    public EncryptionCredential(PublicKey publicKey) { super(publicKey); }
-}
