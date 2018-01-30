ef7099ca4795937c1358ea637577179429a3cf20
Anshul Sirur <anshul.sirur@digital.cabinet-office.gov.uk>
2018-01-30 17:12:02 +0000

    EID-3: Publish metadata to PaaS
    Since the Proxy Node no longer hosts metadata for Stub IDP (Hub) and
    the Connector Node, push the static files to PaaS.
    
    Authors: @vixus0 @cobainc0

--- jenkins/paas.sh
+++ jenkins/paas.sh
@@ -4,5 +4,11 @@ set -eu
 
 . ./jenkins/login_to_paas.sh
 
+# Update metadata
+cp src/main/resources/paas/*.xml metadata/
+pushd metadata/
+  cf push
+popd
+
 ./gradlew -x test pushToPaas
 ./jenkins/acceptance-test.sh
