aae3ada72b13c52ea7d431a01e034f5ce21d7f46
Minno Dang <minh.ngocdang@digital.cabinet-office.gov.uk>
2018-02-05 12:06:05 +0000

    BAU: fixed missing values in stub-idp configs
    @minhngocd

--- local-acceptance.sh
+++ local-acceptance.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
 
 ./shutdown.sh
-./startup.sh --build
+(./startup.sh --build --follow &) > ./logs/docker.log
 
+echo "Waiting for CEF"
 until $(curl --output /dev/null --silent --head --header "Connection: keep-alive" http://localhost:56000/); do
-  echo "Waiting for CEF"
+  echo -n "."
   sleep 1
 done
+echo "CEF started"
 
+echo "Waiting for Proxy Node"
 until $(curl --output /dev/null --silent --head --header "Connection: keep-alive" http://localhost:56016/); do
-  echo "Waiting for Proxy Node"
+  echo -n "."
   sleep 1
 done
+echo "Proxy Node started"
 
+echo "Waiting for Stub IDP"
 until $(curl --output /dev/null --silent --head --header "Connection: keep-alive" http://localhost:56017/); do
-  echo "Waiting for Stub IDP"
+  echo -n "."
   sleep 1
 done
+echo "Stud IDP started"
 
 ./gradlew acceptanceTest
 
