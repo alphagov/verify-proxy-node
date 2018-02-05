2088ef92aa82b8e0e0ce0c2a17727d2be2dbb15c
Minno Dang <minh.ngocdang@digital.cabinet-office.gov.uk>
2018-02-05 16:42:45 +0000

    BAU: updated configurations to work with newest changes to stub-idp
    - We should just be using whatever configuration file stub-idp uses, and
      set values using environment variables to be more flexible with
    changes
    - node-metadata-reverse-proxy was exiting since it was unable to find
      proxy-node-1. Pulled in config file to make it notification specific.
    
    @minhngocd

--- local_eidas_reference/scripts/start-stub-idp.sh
+++ local_eidas_reference/scripts/start-stub-idp.sh
@@ -5,8 +5,6 @@ RUN_DIR=$(pwd -P)
 DIST_DIR="$RUN_DIR/build/distributions"
 ZIP_DIR="$DIST_DIR/$APP_NAME"
 
-CONFIG_FILE="$RUN_DIR/notification_resources/configuration.yml"
-
 pushd "$RUN_DIR" >/dev/null
   ./gradlew clean distZip
 popd >/dev/null
@@ -16,4 +14,4 @@ pushd "$DIST_DIR" >/dev/null
   unzip -q "${APP_NAME}.zip"
 popd >/dev/null
 
-CONFIG_FILE=$CONFIG_FILE "$ZIP_DIR/bin/$APP_NAME"
+"$ZIP_DIR/bin/$APP_NAME"
