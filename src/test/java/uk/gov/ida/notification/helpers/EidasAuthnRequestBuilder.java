46ee79de5e45e3e4556bbe16d91a919888088acb
Minno Dang <minh.ngocdang@digital.cabinet-office.gov.uk>
2018-02-06 16:38:30 +0000

    EIDAS-6: EidasAuthnRequestBuilder is now customisable
    - Able to edit AuthnRequests elements to build different requests for testing
    
    Solo: @minhngocd

--- src/test/java/uk/gov/ida/notification/helpers/EidasAuthnRequestBuilder.java
+++ src/test/java/uk/gov/ida/notification/helpers/EidasAuthnRequestBuilder.java
@@ -1,23 +1,43 @@
 package uk.gov.ida.notification.helpers;
 
 import org.opensaml.saml.saml2.core.AuthnRequest;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import uk.gov.ida.notification.saml.SamlParser;
 
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
 import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.HashMap;
 
 public class EidasAuthnRequestBuilder {
+    private final String EIDAS_AUTHN_REQUEST_XML = "eidas_authn_request.xml";
     private final SamlParser parser;
+    private final Document authnRequestDocument;
+    private final String saml2 = "urn:oasis:names:tc:SAML:2.0:assertion";
+    private final String saml2p = "urn:oasis:names:tc:SAML:2.0:protocol";
+    private final String ds = "http://www.w3.org/2000/09/xmldsig#";
+    private HashMap<String, String> namespaceMap;
 
-    public EidasAuthnRequestBuilder() {
-        try {
-            parser = new SamlParser();
-        } catch (ParserConfigurationException e) {
-            throw new RuntimeException(e);
-        }
+    public EidasAuthnRequestBuilder() throws Exception {
+        parser = new SamlParser();
+        authnRequestDocument = XmlHelpers.readDocumentFromFile(EIDAS_AUTHN_REQUEST_XML);
+        namespaceMap = new HashMap<String, String>(){{
+            put("saml2", saml2);
+            put("saml2p", saml2p);
+            put("ds", ds);
+        }};
     }
 
-    public AuthnRequest build() throws IOException {
-        return parser.parseSamlString(FileHelpers.readFileAsString("eidas_authn_request.xml"));
+    public AuthnRequest build() throws IOException, TransformerException {
+        String authnRequestString = XmlHelpers.serializeDomElementToString(authnRequestDocument.getDocumentElement());
+        return parser.parseSamlString(authnRequestString);
+    }
+
+    public EidasAuthnRequestBuilder withNoIssuer() throws XPathExpressionException {
+        Node node = XmlHelpers.findNodeInDocument(authnRequestDocument, "//saml2:Issuer", namespaceMap);
+        node.getParentNode().removeChild(node);
+        return this;
     }
 }
