e722b40c4106e201428d02e3a1c7dd6e73e0edf4
Simon Worthington <simon.worthington@digital.cabinet-office.gov.uk>
2018-11-13 17:47:33 +0000

    EID-1062 Move Hub Response validators into proxy-node-shared
    - Also add a switch to HubResponseValidator that ignores
      the 'Response matches Request ID' validation check.

--- proxy-node-shared/src/main/java/uk/gov/ida/notification/saml/validation/HubResponseValidator.java
+++ proxy-node-shared/src/main/java/uk/gov/ida/notification/saml/validation/HubResponseValidator.java
@@ -3,6 +3,8 @@ package uk.gov.ida.notification.saml.validation;
 import org.opensaml.saml.saml2.core.Assertion;
 import org.opensaml.saml.saml2.core.Response;
 import uk.gov.ida.notification.exceptions.hubresponse.InvalidHubResponseException;
+import uk.gov.ida.notification.saml.validation.components.LoaValidator;
+import uk.gov.ida.notification.saml.validation.components.RequestIdWatcher;
 import uk.gov.ida.notification.saml.validation.components.ResponseAttributesValidator;
 import uk.gov.ida.saml.core.validation.SamlTransformationErrorException;
 import uk.gov.ida.saml.hub.validators.response.idp.IdpResponseValidator;
@@ -13,10 +15,25 @@ public class HubResponseValidator {
 
     private final IdpResponseValidator idpResponseValidator;
     private final ResponseAttributesValidator responseAttributesValidator;
+    private final LoaValidator loaValidator;
+    private final RequestIdWatcher requestIdWatcher;
 
-    public HubResponseValidator(IdpResponseValidator idpResponseValidator, ResponseAttributesValidator responseAttributesValidator) {
+    public HubResponseValidator(
+        IdpResponseValidator idpResponseValidator,
+        ResponseAttributesValidator responseAttributesValidator,
+        LoaValidator loaValidator) {
+        this(idpResponseValidator, responseAttributesValidator, loaValidator, null);
+        }
+
+    public HubResponseValidator(
+        IdpResponseValidator idpResponseValidator,
+        ResponseAttributesValidator responseAttributesValidator,
+        LoaValidator loaValidator,
+        RequestIdWatcher requestIdWatcher) {
         this.idpResponseValidator = idpResponseValidator;
         this.responseAttributesValidator = responseAttributesValidator;
+        this.loaValidator = loaValidator;
+        this.requestIdWatcher = requestIdWatcher;
     }
 
     public ValidatedResponse getValidatedResponse() {
@@ -30,10 +47,17 @@ public class HubResponseValidator {
     public void validate(Response response) {
         try {
             idpResponseValidator.validate(response);
+            if (requestIdWatcher != null) requestIdWatcher.validateSeenRequestFor(response);
+
             Assertion matchingDatasetAssertion = getValidatedAssertions()
                 .getMatchingDatasetAssertion()
                 .orElseThrow(() -> new InvalidHubResponseException("Missing Matching Dataset Assertions"));
             responseAttributesValidator.validate(matchingDatasetAssertion.getAttributeStatements().get(0));
+
+            Assertion authnContextAssertion = getValidatedAssertions()
+                .getAuthnStatementAssertion()
+                .orElseThrow(() -> new InvalidHubResponseException("Missing Authn Statement Assertion"));
+            loaValidator.validate(authnContextAssertion.getAuthnStatements().get(0).getAuthnContext());
         } catch (SamlTransformationErrorException exception) {
             throw new InvalidHubResponseException(exception.getMessage(), exception);
         }
