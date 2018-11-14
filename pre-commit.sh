67ad5a4952b9599804c35a2643d6f96e12cf578a
James Howes <james.howes@digital.cabinet-office.gov.uk>
2018-11-14 13:49:14 +0000

    EID-1075 - Run acceptance tests from pre-commit
    - Change pre-commit to start services and run acceptance tests
    - Wait for services to start before running acceptance tests
    - Remove old acceptance tests code/scripts
    
    Co-authored-by: Andrew Chong <andrew.chong@digital.cabinet-office.gov.uk>

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-./docker-gradle clean test
+./docker-gradle clean -x test
+
+./startup.sh
+
+echo "Waiting 30 seconds for services."
+sleep 30
+
+echo ===========================================================
+echo Running acceptance tests
+echo ===========================================================
+
+pushd notification-acceptance-tests > /dev/null
+  $(pwd)/pre-commit.sh
+popd > /dev/null
