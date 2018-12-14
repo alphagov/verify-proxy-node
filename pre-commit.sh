6aa12037f5b0d87fc70cad4dd922e5607adb3e20
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2018-12-14 12:51:35 +0000

    updates acceptance test url to use minikube
    We want the pre-commit.sh script to run against the minikube deployment,
    this adds the STUB_CONNECTOR_URL env var to allow passing through the
    entrypoint url to the test

--- pre-commit.sh
+++ pre-commit.sh
@@ -10,6 +10,8 @@ echo ===========================================================
 echo Running acceptance tests
 echo ===========================================================
 
+export STUB_CONNECTOR_URL="http://$(minikube ip):31100/Request"
+
 pushd proxy-node-acceptance-tests > /dev/null
   $(pwd)/pre-commit.sh
 popd > /dev/null
