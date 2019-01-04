22b47782c00850d2c567aa182285235d063d2749
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2019-01-04 11:51:49 +0000

    ensures token dir exists in softhsm init
    if the token dir does not exist then the init-token and key import will
    fail with an obscure "PKCS11 function C_Initialize failed" error.

--- proxy-node-translator/src/dist/init.sh
+++ proxy-node-translator/src/dist/init.sh
@@ -8,6 +8,9 @@ set -eu
 base64 --decode < /app/keys/SIGNING_PRIVATE_KEY_SOFTHSM > /app/signing_private_key.p8
 base64 --decode < /app/keys/SIGNING_CERT > /app/signing_cert.crt
 
+# ensure token store dir exists
+mkdir -p /var/lib/softhsm/tokens
+
 # Init softHSM slot
 pkcs11-tool --module $SOFT_HSM_LIB_PATH --init-token --slot $SOFT_HSM_SIGNING_KEY_SLOT --so-pin $SOFT_HSM_SIGNING_KEY_SO_PIN --init-pin --pin $SOFT_HSM_SIGNING_KEY_PIN --label $SOFT_HSM_SIGNING_KEY_LABEL
 
