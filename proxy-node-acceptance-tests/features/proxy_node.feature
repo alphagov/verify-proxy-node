1eac7972f1f5d07c0cce4841af0d633298a7dfb6
Tom Rosier <tom.rosier@digital.cabinet-office.gov.uk>
2019-01-24 20:10:31 +0000

    EID-1243: Added some more acceptance tests, and tidied up some stuff in STUB Connector

--- proxy-node-acceptance-tests/features/proxy_node.feature
+++ proxy-node-acceptance-tests/features/proxy_node.feature
@@ -4,3 +4,20 @@ Feature: proxy-node feature
         Given the user is at Stub Connector
         And they login as "stub-idp-demo"
         Then they should arrive at the success page
+
+    Scenario: IDP Generates Authn Failure
+        Given the user is at Stub Connector
+        And the stub idp supplies a "Authn Failure"
+        Then the user should be presented with an hub error page
+
+    Scenario: IDP Generates Authn Failure
+        Given the stub connector supplies a bad authn request
+        Then the user should be presented with an authn error page
+
+    Scenario: Show 404 if page doesnt exist
+        Given the user accesses a invalid page
+        Then the user should be presented with an '404' page
+
+    Scenario: Show 405 if route is not accessible
+        Given the user accesses a route they shouldnt
+        Then the user should be presented with an '405' page
--- proxy-node-acceptance-tests/features/step_definitions/user_steps.rb
+++ proxy-node-acceptance-tests/features/step_definitions/user_steps.rb
@@ -3,7 +3,11 @@ require 'uri'
 require 'securerandom'
 
 Given('the user is at Stub Connector') do
-  visit(ENV.fetch('STUB_CONNECTOR_URL'))
+  visit(ENV.fetch('PROXY_NODE_URL') + ':31100/Request')
+end
+
+Given("the stub connector supplies a bad authn request") do
+  visit(ENV.fetch('PROXY_NODE_URL') + ':31100/BadRequest')
 end
 
 Given('they login as {string}') do |username|
@@ -13,9 +17,37 @@ Given('they login as {string}') do |username|
   click_on('I Agree')
 end
 
+Given("the stub idp supplies a {string}") do |error_to_throw|
+  click_on(error_to_throw)
+end
+
+Given("the user accesses a invalid page") do
+  visit(ENV.fetch('PROXY_NODE_URL') + ':31200/Request')
+end
+
+Given("the user accesses a route they shouldnt") do
+  visit(ENV.fetch('PROXY_NODE_URL') + ':31200/SAML2/SSO/Response/POST/')
+end
+
 Then('they should arrive at the success page') do
   assert_text('Response successfully received')
   assert_text('Jack Cornelius')
   assert_text('Bauer')
   assert_text('1984-02-29')
+end
+
+Then("the user should be presented with an authn error page") do
+  assert_text('Sorry, something went wrong')
+  assert_text('Error handling authn request.')
+end
+
+Then("the user should be presented with an hub error page") do
+  assert_text('Sorry, something went wrong')
+  assert_text('Error handling hub response.')
+end
+
+
+Then("the user should be presented with an {string} page") do |http_error_code|
+  assert_text('Sorry, something went wrong')
+  assert_text('HTTP ' + http_error_code)
 end
