0de7b943e8c0341e35ac6b049ec52cf09dde8520
James Howes <james.howes@digital.cabinet-office.gov.uk>
2019-01-29 12:04:21 +0000

    EID-1255 - Remove validation to check expected recipient id
    - Update bundle in pki
    - Remove validation to check the expected recipient id of the subject. The recipient
    of the subject is set for the hub by the idp and doesn't change in the hub so when it
    reaches the proxy node, the recipient id of the subject is still the hub. Therefore
    this validation will fail when connected to the Hub.
    - This is the same behaviour as the MSA.

--- proxy-node-shared/src/main/java/uk/gov/ida/notification/saml/deprecate/AssertionSubjectConfirmationValidator.java
+++ proxy-node-shared/src/main/java/uk/gov/ida/notification/saml/deprecate/AssertionSubjectConfirmationValidator.java
@@ -4,12 +4,12 @@ import org.opensaml.saml.saml2.core.SubjectConfirmation;
 import org.opensaml.saml.saml2.core.SubjectConfirmationData;
 import uk.gov.ida.saml.core.validation.SamlTransformationErrorException;
 import uk.gov.ida.saml.core.validation.SamlValidationSpecificationFailure;
+
 public class AssertionSubjectConfirmationValidator extends BasicAssertionSubjectConfirmationValidator {
 
     public void validate(
             SubjectConfirmation subjectConfirmation,
-            String requestId,
-            String expectedRecipientId) {
+            String requestId) {
 
         super.validate(subjectConfirmation);
 
@@ -17,10 +17,6 @@ public class AssertionSubjectConfirmationValidator extends BasicAssertionSubject
 
         if (!subjectConfirmationData.getInResponseTo().equals(requestId)) {
             SamlValidationSpecificationFailure failure = SamlTransformationErrorFactory.notMatchInResponseTo(subjectConfirmationData.getInResponseTo(), requestId);
-            throw new SamlTransformationErrorException(failure.getErrorMessage(), failure.getLogLevel());
-        }
-        if (!subjectConfirmationData.getRecipient().equals(expectedRecipientId)) {
-            SamlValidationSpecificationFailure failure = SamlTransformationErrorFactory.incorrectRecipientFormat(subjectConfirmationData.getRecipient(), expectedRecipientId);
             throw new SamlTransformationErrorException(failure.getErrorMessage(), failure.getLogLevel());
         }
     }
