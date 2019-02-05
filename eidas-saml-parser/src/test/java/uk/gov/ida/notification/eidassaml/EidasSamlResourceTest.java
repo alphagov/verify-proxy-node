d7dcc50b9bac2c741675ab5b1798cd17d0739f3c
Anshul Sirur <vixus0@gmail.com>
2019-02-05 17:14:21 +0000

    EID-1308: Extract ID and Issuer from AuthnRequest
    Base64-decodes and parses the AuthnRequest before extracting the ID and
    Issuer. No validation of the message is being performed at this stage,
    that will be implemented in EID-1304/1305.

--- eidas-saml-parser/src/test/java/uk/gov/ida/notification/eidassaml/EidasSamlResourceTest.java
+++ eidas-saml-parser/src/test/java/uk/gov/ida/notification/eidassaml/EidasSamlResourceTest.java
@@ -1,8 +1,14 @@
 package uk.gov.ida.notification.eidassaml;
 
 import io.dropwizard.testing.junit.ResourceTestRule;
+import org.glassfish.jersey.internal.util.Base64;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.opensaml.core.config.InitializationService;
+import org.opensaml.saml.saml2.core.AuthnRequest;
+import org.opensaml.saml.saml2.core.Issuer;
+import se.litsec.opensaml.utils.ObjectUtils;
 
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
@@ -15,10 +21,21 @@ public class EidasSamlResourceTest {
         .addResource(new EidasSamlResource())
         .build();
 
+    @Before
+    public void setup() throws Exception {
+        InitializationService.initialize();
+    }
+
     @Test
-    public void shouldReturnRequestIdAndIssuer() {
+    public void shouldReturnRequestIdAndIssuer() throws Exception {
+        AuthnRequest authnRequest = ObjectUtils.createSamlObject(AuthnRequest.class);
+        Issuer issuer = ObjectUtils.createSamlObject(Issuer.class);
+        issuer.setValue("issuer");
+        authnRequest.setID("request_id");
+        authnRequest.setIssuer(issuer);
+
         RequestDto request = new RequestDto();
-        request.authnRequest = "pantoufles";
+        request.authnRequest = Base64.encodeAsString(ObjectUtils.toString(authnRequest));
 
         ResponseDto response = resources.target("/eidasAuthnRequest")
             .request(MediaType.APPLICATION_JSON_TYPE)
@@ -26,6 +43,6 @@ public class EidasSamlResourceTest {
             .readEntity(ResponseDto.class);
 
         assertEquals(response.requestId, "request_id");
-        assertEquals(response.requestId, "request_id");
+        assertEquals(response.issuer, "issuer");
     }
 }
