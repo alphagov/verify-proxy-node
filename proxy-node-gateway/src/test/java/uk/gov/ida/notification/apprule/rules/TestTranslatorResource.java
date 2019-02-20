67f8d54c5ac83a729902b2114c7098fdbddc008a
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2019-02-20 11:09:35 +0000

    EID-1337 Refactor test service resources for clientrules
    The test resources for the VSP and the Translator were both creating
    saml objects to be serialized and sent as responses. This is unnecessary
    for the gateway as it is unaware of saml. We can instead just provide
    strings and make sure they are passed correctly by the gateway.

--- proxy-node-gateway/src/test/java/uk/gov/ida/notification/apprule/rules/TestTranslatorResource.java
+++ proxy-node-gateway/src/test/java/uk/gov/ida/notification/apprule/rules/TestTranslatorResource.java
@@ -1,61 +1,23 @@
 package uk.gov.ida.notification.apprule.rules;
 
-import org.opensaml.saml.saml2.core.*;
-import org.opensaml.security.credential.Credential;
-import uk.gov.ida.notification.helpers.EidasAuthnRequestBuilder;
-import uk.gov.ida.notification.saml.SamlFormMessageType;
-import uk.gov.ida.notification.saml.SamlObjectMarshaller;
-import uk.gov.ida.saml.core.test.TestCredentialFactory;
-import uk.gov.ida.saml.core.test.builders.*;
+import org.glassfish.jersey.internal.util.Base64;
+import uk.gov.ida.notification.contracts.HubResponseTranslatorRequest;
+import uk.gov.ida.notification.shared.Urls;
 
-import javax.ws.rs.*;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path(Urls.TranslatorUrls.TRANSLATOR_ROOT)
+@Produces(MediaType.APPLICATION_JSON)
 public class TestTranslatorResource {
 
+    public static final String SAML_BLOB = Base64.encodeAsString("I'm also going to be a SAML blob!");
+
     @POST
-    @Path("Urls.TranslatorUrls.TRANSLATE_HUB_RESPONSE_PATH")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(MediaType.APPLICATION_XML)
-    public String hubResponse(@FormParam(SamlFormMessageType.SAML_RESPONSE) String encryptedHubResponse) throws Throwable {
-        Credential encryptingCredential = new TestCredentialFactory(
-                TEST_RP_PUBLIC_ENCRYPTION_CERT,
-                TEST_RP_PRIVATE_ENCRYPTION_KEY
-        ).getDecryptingCredential();
-
-        AttributeValue firstnameValue = PersonNameAttributeValueBuilder
-                .aPersonNameValue()
-                .withValue("Jazzy Harold")
-                .withVerified(true)
-                .build();
-
-        Attribute firstname = PersonNameAttributeBuilder_1_1.aPersonName_1_1()
-                .addValue(firstnameValue)
-                .buildAsFirstname();
-
-        AttributeStatement attributeStatement = AttributeStatementBuilder
-                .anAttributeStatement()
-                .addAttribute(firstname)
-                .build();
-
-        AuthnStatement authnStatement = AuthnStatementBuilder.anAuthnStatement().build();
-
-        EncryptedAssertion encryptedAssertion = AssertionBuilder
-                .anAssertion()
-                .withId("encryptedAssertion")
-                .addAttributeStatement(attributeStatement)
-                .addAuthnStatement(authnStatement)
-                .buildWithEncrypterCredential(encryptingCredential);
-
-        Response response = ResponseBuilder
-                .aResponse()
-                .withInResponseTo(new EidasAuthnRequestBuilder().build().getID())
-                .addEncryptedAssertion(encryptedAssertion)
-                .build();
-
-        SamlObjectMarshaller marshaller = new SamlObjectMarshaller();
-        String samlMessage = marshaller.transformToString(response);
-        return samlMessage;
+    @Path(Urls.TranslatorUrls.TRANSLATE_HUB_RESPONSE_PATH)
+    public String hubResponse(HubResponseTranslatorRequest hubResponseTranslatorRequest) {
+        return SAML_BLOB;
     }
 }
