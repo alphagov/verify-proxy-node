4eb64e17c753fd29cb8e9687332ed9f30bef09d9
dbes-gds <38218115+dbes-gds@users.noreply.github.com>
2019-03-06 15:54:42 +0000

    EID-1358: Log eIDAS Response Attributes
    - Log select attributes from the eIDAS response after translation and response generation to MDC.
    - AppRule tests to verify logging and logging output to console in json format.

---
+++ proxy-node-translator/src/main/java/uk/gov/ida/notification/translator/logging/HubResponseTranslatorLoggerHelper.java
@@ -0,0 +1,36 @@
+package uk.gov.ida.notification.translator.logging;
+
+import org.opensaml.saml.saml2.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+public class HubResponseTranslatorLoggerHelper {
+
+    private static final Logger log = LoggerFactory.getLogger(HubResponseTranslatorLoggerHelper.class);
+    public static final String EIDAS_RESPONSE_LOGGER_MESSAGE = "eIDAS Response Attributes";
+
+    public interface HubResponseTranslatorLoggerAttributes {
+        String EIDAS_RESPONSE_ID = "eidasResponse.id";
+        String EIDAS_RESPONSE_IN_RESPONSE_TO = "eidasResponse.inResponseTo";
+        String EIDAS_RESPONSE_DESTINATION = "eidasResponse.destination";
+        String EIDAS_RESPONSE_ISSUER = "eidasResponse.issuer";
+    }
+
+    public static void logEidasResponse(Response eidasResponse) {
+        try {
+            MDC.put(HubResponseTranslatorLoggerAttributes.EIDAS_RESPONSE_ID, eidasResponse.getID() != null ? eidasResponse.getID() : "");
+            MDC.put(HubResponseTranslatorLoggerAttributes.EIDAS_RESPONSE_IN_RESPONSE_TO, eidasResponse.getInResponseTo() != null ? eidasResponse.getInResponseTo() : "");
+            MDC.put(HubResponseTranslatorLoggerAttributes.EIDAS_RESPONSE_DESTINATION, eidasResponse.getDestination() != null ? eidasResponse.getDestination() : "");
+            MDC.put(HubResponseTranslatorLoggerAttributes.EIDAS_RESPONSE_ISSUER, eidasResponse.getIssuer() != null ? eidasResponse.getIssuer().getValue() : "");
+            log.info(EIDAS_RESPONSE_LOGGER_MESSAGE);
+        } finally {
+            MDC.clear();
+        }
+    }
+
+    public static void logHashedResponseDetails(String requestId, String destination, String hashedDetails) {
+        log.info(String.format("[eIDAS Response HASH] received for hub authn request ID '%s', destination '%s', hashedEidasDetails '%s'",
+                requestId, destination, hashedDetails));
+    }
+}
