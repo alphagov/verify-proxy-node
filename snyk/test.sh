1b600d0c2dff6b1d4e526f3e7127e19fd1cb9c42
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2019-03-12 12:00:58 +0000

    EID-1381 Integrate Snyk CLI with Travis
    Snyk doesn't play very nicely with gradle sub-projects that use
    configurations like we do. As such we can't use the Github integration.
    We _can_ use the CLI however with Travis to get a similar result.
    
    We can't test individual sub projects, only the gradle configurations.
    We configurations to be defined are in one place and source by the test
    and monitor scripts to ensure consistency.
    
    The monitor script is run as part of the `after_success` phase of the
    Travis build. This will only be run if all tests pass, as well as the
    snyk test. If the monitor script fails for some reason the build won't
    be marked as failure.
    
    If the test script fails, the build will be marked as a failure. If any
    of the configurations show a security issue, the script will fail. At
    this point you can fix the issue. If the issue can't be fixed, it can eb
    ignored via the snyk UI.

---
+++ snyk/test.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# If a new configuration is added it will need adding to `snyk_configurations.sh`.
+# This script is intended to be used by a CI server.
+
+exit_code=0
+
+function print_banner() {
+    echo "######################################################################"
+    echo "### Testing dependencies for $1 gradle configuration"
+    echo "######################################################################"
+}
+
+function test_configuration() {
+    local config=$1;
+    print_banner "$config"
+    # The sub-project specified here is irrelevant. The configuration will still be tested regardless of if the
+    # sub-project actually uses it.
+    snyk test --gradle-sub-project=proxy-node-translator -- --configuration="$config"
+    if [[ $? -gt 0 ]]; then
+        exit_code=1
+    fi
+}
+
+source snyk/configurations.sh
+
+for configuration in $CONFIGURATIONS; do
+   test_configuration $configuration
+done
+
+exit $exit_code
