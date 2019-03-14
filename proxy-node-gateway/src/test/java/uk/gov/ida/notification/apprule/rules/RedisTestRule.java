e8eea1bc4e2751f0f0dd5f2254db752fa64e615b
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2019-03-14 17:41:11 +0000

    EID-1357 Use version 5.0.3 of redis
    The standard version used with embedded-reddis is v2.8.19. We're using
    the latest version from Dockerhub which at the time of writing is 5.0.2.
    
    Two binaries have been added, one compiled on a mac and the other
    compiled on trusty. Travis runs on Trusty hence the need for both.

--- proxy-node-gateway/src/test/java/uk/gov/ida/notification/apprule/rules/RedisTestRule.java
+++ proxy-node-gateway/src/test/java/uk/gov/ida/notification/apprule/rules/RedisTestRule.java
@@ -2,13 +2,44 @@ package uk.gov.ida.notification.apprule.rules;
 
 import org.junit.rules.ExternalResource;
 import redis.embedded.Redis;
+import redis.embedded.RedisExecProvider;
 import redis.embedded.RedisServer;
+import redis.embedded.exceptions.EmbeddedRedisException;
+import redis.embedded.util.Architecture;
+import redis.embedded.util.OS;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 
 public class RedisTestRule extends ExternalResource {
     private Redis redis;
 
     public RedisTestRule(int port) {
-        redis = new RedisServer(port);
+        File redisServerMac;
+        File redisServerTrusty;
+        try {
+            URL redisServerMacResource = RedisTestRule.class.getResource("/redis-server-mac-5.0.3");
+            redisServerMac = Paths.get(redisServerMacResource.toURI()).toFile();
+
+            URL redisServerTrustyResource = RedisTestRule.class.getResource("/redis-server-trusty-5.0.3");
+            redisServerTrusty = Paths.get(redisServerTrustyResource.toURI()).toFile();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+        RedisExecProvider customProvider = RedisExecProvider.defaultProvider()
+            .override(OS.UNIX, redisServerTrusty.getAbsolutePath())
+            .override(OS.MAC_OS_X, Architecture.x86_64, redisServerMac.getAbsolutePath());
+
+        try {
+            redis = new RedisServer(customProvider, port);
+        } catch (IOException e) {
+            throw new EmbeddedRedisException(e.getMessage());
+        }
+
     }
 
     @Override
