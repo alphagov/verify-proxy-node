6bd947a3d32254ddaef29ab0bc5f68b85fd77991
Sam Crang <sam.crang@gmail.com>
2019-03-21 16:40:13 +0000

    bump

--- deploy.sh
+++ deploy.sh
@@ -18,12 +18,12 @@ helm template proxy-node-chart/ \
 	--set "hsm.user=cu1" \
 	--set "hsm.base64EncodedPassword=$(aws ssm get-parameter --query Parameter.Value --output text --with-decryption --name /hsm/users/cu/1/password | tr -d '\n' | base64)" \
 	--set "hsm.customerCA=$(aws ssm get-parameter --query Parameter.Value --output text --with-decryption --name /hsm/customerCA)" \
-	--set "hsm.image.tag=v0.0.0-dev-e48b1eb" \
-	--set "esp.image.tag=v0.0.0-dev-e48b1eb" \
-	--set "gateway.image.tag=v0.0.0-dev-e48b1eb" \
-	--set "translator.image.tag=v0.0.0-dev-e48b1eb" \
+	--set "hsm.image.tag=v0.0.0-dev-78830bd" \
+	--set "esp.image.tag=v0.0.0-dev-78830bd" \
+	--set "gateway.image.tag=v0.0.0-dev-78830bd" \
+	--set "translator.image.tag=v0.0.0-dev-78830bd" \
 	--set "stubConnector.enabled=true" \
-	--set "stubConnector.image.tag=v0.0.0-dev-e48b1eb" \
+	--set "stubConnector.image.tag=v0.0.0-dev-78830bd" \
 	--set "vmc.image.tag=v0.0.0-dev-20c5227" \
 	--set "vsp.image.tag=v0.0.0-dev-2503371" \
 	| kubectl -n "${RELEASE}-proxy-node" apply -f -
