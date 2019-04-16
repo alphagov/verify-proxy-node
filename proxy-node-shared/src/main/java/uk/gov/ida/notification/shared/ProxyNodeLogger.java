bf85f1069b0daf9b970daff8f66b23e7d825483a
Phil Miller <phillip.miller@digital.cabinet-office.gov.uk>
2019-04-16 17:15:03 +0100

    WIP log the correct class and method invoking the logging call

--- proxy-node-shared/src/main/java/uk/gov/ida/notification/shared/ProxyNodeLogger.java
+++ proxy-node-shared/src/main/java/uk/gov/ida/notification/shared/ProxyNodeLogger.java
@@ -10,17 +10,23 @@ import java.util.logging.Logger;
 public class ProxyNodeLogger {
 
     private static final Logger LOG = Logger.getLogger(ProxyNodeLogger.class.getName());
+    private static final int FRAMES_IN_THIS_CLASS = 2;
 
     public void addContext(ProxyNodeMDCKey key, String value) {
         MDC.put(key.name(), value);
     }
 
     public void log(Level level, String message) {
-        log(level, () -> message);
+        Optional<StackWalker.StackFrame> callingFrame = getCallingStackFrame();
+        logPrivate(level, () -> message, callingFrame);
     }
 
     public void log(Level level, Supplier<String> message) {
         Optional<StackWalker.StackFrame> callingFrame = getCallingStackFrame();
+        logPrivate(level, message, callingFrame);
+    }
+
+    private void logPrivate(Level level, Supplier<String> message, Optional<StackWalker.StackFrame> callingFrame) {
         callingFrame.ifPresent(f -> {
             addContext(ProxyNodeMDCKey.LOG_LOCATION, String.format("%s.%s", f.getClassName(), f.getMethodName()));
             LOG.log(level, message);
@@ -29,6 +35,7 @@ public class ProxyNodeLogger {
     }
 
     private Optional<StackWalker.StackFrame> getCallingStackFrame() {
-        return StackWalker.getInstance().walk(s -> s.skip(1).findFirst());
+        return StackWalker.getInstance().walk(s -> s.skip(FRAMES_IN_THIS_CLASS).findFirst());
     }
+
 }
