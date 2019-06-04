d83fb8a40510716f47b2cfc50ac45c4b564219a7
Phil Miller <phillip.miller@digital.cabinet-office.gov.uk>
2019-06-04 14:28:09 +0100

    Acceptance tests for EID-1517

--- proxy-node-acceptance-tests/run-staging-tests.sh
+++ proxy-node-acceptance-tests/run-staging-tests.sh
@@ -4,8 +4,8 @@ set -eu
 echo "Before Docker compose build"
 docker-compose build
 echo "Docker compose build"
-export PROXY_NODE_URL="https://test-proxy-node.staging.verify.govsvc.uk"
-export STUB_CONNECTOR_URL="https://test-connector.staging.verify.govsvc.uk"
+export PROXY_NODE_URL="https://test-proxy-node.london.verify.govsvc.uk"
+export STUB_CONNECTOR_URL="https://test-connector.london.verify.govsvc.uk"
 export STUB_IDP_USER="stub-idp-demo-one"
 docker-compose up --abort-on-container-exit | grep acceptance-tests_1 --colour=never
 docker cp $(docker ps -a -q -f name="acceptance-tests"):/testreport .
