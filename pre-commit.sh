b1dfec0353e1f20e2cec14991d270b3b4eb98daf
Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>
2019-06-12 12:55:45 +0100

    BAU: Test generating dependency files for GitHub security alerts
    Move poms to `libs` directory
    
    Author: @CharlesIC

--- pre-commit.sh
+++ pre-commit.sh
@@ -3,9 +3,9 @@ set -e
 
 ./gradlew clean test
 
-if ! git diff -s --exit-code dependencies/*/pom.xml; then
+if ! git diff -s --exit-code libs/*/pom.xml; then
   echo '⚠️ Remember to commit and push updated project dependencies files (pom.xml) ⚠️'
-  git add dependencies/*/pom.xml
+  git add libs/*/pom.xml
 fi
 
 ./startup.sh --build
