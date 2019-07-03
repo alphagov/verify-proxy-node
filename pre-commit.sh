d270a704e8f1b44b21f186e19a1f68ed52907053
Phil Miller <phillip.miller@digital.cabinet-office.gov.uk>
2019-07-03 16:34:57 +0100

    Removed scripts related to starting a local Proxy Node

--- pre-commit.sh
+++ pre-commit.sh
@@ -8,15 +8,3 @@ if ! git diff -s --exit-code libs/*/pom.xml; then
   git add libs/*/pom.xml
 fi
 
-./startup.sh --build
-
-
-echo ===========================================================
-echo Running acceptance tests
-echo ===========================================================
-
-export STUB_CONNECTOR_URL="http://$(minikube ip):31100/Request"
-
-pushd proxy-node-acceptance-tests > /dev/null
-  $(pwd)/run-tests-with-docker.sh
-popd > /dev/null
--- shutdown.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ueo pipefail
-
-PN_PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-minikube stop
-minikube delete
