c864252bee9c324df90c1f10bc4c5b42c91cc671
Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>
2019-07-10 19:26:32 +0100

    BAU: Move everything local start-up related to one directory
    Step 1: Move everything local startup related to its own directory.
    These scripts are not currently working but will be kept for reference for the time being until we have a working solution.
    We may be able to reuse some of the existing stuff so it's worth keeping it temporarily.
    
    Author: @CharlesIC

--- local-startup/pre-commit.sh
+++ local-startup/pre-commit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-./gradlew clean test
+../gradlew clean test
 
 if ! git diff -s --exit-code libs/*/pom.xml; then
   echo '⚠️ Remember to commit and push updated project dependencies files (pom.xml) ⚠️'
