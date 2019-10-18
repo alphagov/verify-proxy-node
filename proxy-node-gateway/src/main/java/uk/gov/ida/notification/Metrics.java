d01d25bc3bd997208c36ea2bb4715dafc9d2393d
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2019-10-18 15:40:30 +0100

    fix metric names and descriptions
    Counters should have the `_total` suffix:
    https://prometheus.io/docs/practices/naming/
    
    Tools like `promtool check http` and grafana's explore mode will
    complain if counters don't adhere to this convention.
    
    Also, don't use Title Case, use sentence case.  I couldn't find a
    brilliant reference for this but the design system typography guide
    says that headings should use sentence case:
    https://design-system.service.gov.uk/styles/typography/#headings and I
    think that our metric descriptions should match this.

--- proxy-node-gateway/src/main/java/uk/gov/ida/notification/Metrics.java
+++ proxy-node-gateway/src/main/java/uk/gov/ida/notification/Metrics.java
@@ -7,23 +7,23 @@ public class Metrics {
     private static final String LABEL_PREFIX = "verify_proxy_node";
 
     public static final Counter REQUESTS = Counter.build(
-            LABEL_PREFIX + "_requests",
-            "Number of eIDAS SAML Requests To Verify Proxy Node ")
+            LABEL_PREFIX + "_requests_total",
+            "Number of eIDAS SAML requests to Verify Proxy Node ")
             .register();
 
     public static final Counter REQUESTS_SUCCESSFUL = Counter.build(
-            LABEL_PREFIX + "_requests_successful",
-            "Number of Successful eIDAS SAML Requests To Verify Proxy Node")
+            LABEL_PREFIX + "_successful_requests_total",
+            "Number of successful eIDAS SAML requests to Verify Proxy Node")
             .register();
 
     public static final Counter RESPONSES = Counter.build(
-            LABEL_PREFIX + "_responses",
-            "Number of eIDAS SAML Responses To Verify Proxy Node")
+            LABEL_PREFIX + "_responses_total",
+            "Number of eIDAS SAML responses to Verify Proxy Node")
             .register();
 
     public static final Counter RESPONSES_SUCCESSFUL = Counter.build(
-            LABEL_PREFIX + "_responses_successful",
-            "Number of Successful eIDAS SAML Responses To Verify Proxy Node")
+            LABEL_PREFIX + "_successful_responses_total",
+            "Number of successful eIDAS SAML responses To Verify Proxy Node")
             .register();
 
 }
