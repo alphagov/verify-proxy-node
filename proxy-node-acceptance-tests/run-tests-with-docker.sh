f288c989327cace8f4bb5214e5c445b15802cffc
AmritSidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2020-05-28 15:57:24 +0100

    wip

--- proxy-node-acceptance-tests/run-tests-with-docker.sh
+++ proxy-node-acceptance-tests/run-tests-with-docker.sh
@@ -4,9 +4,12 @@ set -eu
 echo "Before Docker compose build"
 docker-compose build
 echo "Docker compose build"
-export PROXY_NODE_URL="http://$(minikube ip):31200"
-export STUB_CONNECTOR_URL="http://$(minikube ip):31100"
-export STUB_IDP_USER=""
-docker-compose up --abort-on-container-exit | grep acceptance-tests_1 --colour=never
-docker cp $(docker ps -a -q -f name="acceptance-tests"):/testreport .
+export PROXY_NODE_URL="http://localhost:6600/"
+export STUB_CONNECTOR_URL="http://localhost:6610/"
+export STUB_IDP_USER="stub-idp-demo-one"
+docker-compose up -d selenium-hub
+
+echo
+docker-compose up --abort-on-container-exit acceptance-tests | grep acceptance-tests_1 --colour=never
+#docker cp $(docker ps -a -q -f name="acceptance-tests"):/testreport .
 docker-compose down
